6aee49e5f55eae0089fa23f7ae2978a3cdecc978
codecrafters-2023 <145508415+codecrafters-2023@users.noreply.github.com>
2025-02-04 20:58:48 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -16,14 +16,14 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.elasticemail.com'; // Replace with your SMTP server
     $mail->SMTPAuth = true;
-    $mail->Username = 'gurbir99156@gmail.com'; // Your SMTP email
-    $mail->Password = 'C6EA121AC89C1F28C30BE579664BE245BB25'; // SMTP password or App Password
+    $mail->Username = ''; // Your SMTP email
+    $mail->Password = ''; // SMTP password or App Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use TLS encryption
     $mail->Port = 2525; // Port for TLS
 
     // Email Content
-    $mail->setFrom('gurbir99156@gmail.com', 'Gurbir Singh'); // Sender's email and name
-    $mail->addAddress('codecrafters@gurmaanitservices.com'); // Recipient's email
+    $mail->setFrom('', 'abc'); // Sender's email and name
+    $mail->addAddress(''); // Recipient's email
     $mail->Subject = 'Test Email from PHPMailer';
     $mail->Body = 'This is a test email sent using PHPMailer without Composer.';
 
